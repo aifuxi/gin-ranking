e2c7794d3dfdd8c3e83b7b354c8bdb54d5628c05
aifuxi <aifuxi.js@gmail.com>
2023-10-18 00:48:35 +0800

    feat: create response with options mode

--- controller/common.go
+++ controller/common.go
@@ -6,12 +6,57 @@ import (
 )
 
 type Response struct {
-	Code int `json:"code"`
-	Msg  any `json:"msg"`
-	Data any `json:"data"`
+	Code  int `json:"code"`
+	Msg   any `json:"msg"`
+	Data  any `json:"data,omitempty"`
+	Error any `json:"error,omitempty"`
+	Total int `json:"total,omitempty"`
 }
 
-func ReturnSuccess(c *gin.Context, resp *Response) {
+type Option func(resp *Response)
 
-	c.JSON(http.StatusOK, resp)
+func WithCode(code int) Option {
+	return func(resp *Response) {
+		resp.Code = code
+	}
+}
+
+func WithMsg(msg string) Option {
+	return func(resp *Response) {
+		resp.Msg = msg
+	}
+}
+
+func WithTotal(total int) Option {
+	return func(resp *Response) {
+		resp.Total = total
+	}
+}
+
+func WithData(data any) Option {
+	return func(resp *Response) {
+		resp.Data = data
+	}
+}
+
+func WithError(err any) Option {
+	return func(resp *Response) {
+		resp.Error = err
+	}
+}
+
+func ReturnResponse(c *gin.Context, opts ...Option) {
+	resp := &Response{
+		Code:  0,
+		Msg:   "success",
+		Data:  nil,
+		Error: nil,
+		Total: 0,
+	}
+
+	for _, opt := range opts {
+		opt(resp)
+	}
+
+	c.JSON(http.StatusOK, *resp)
 }
